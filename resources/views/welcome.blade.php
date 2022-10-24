f6d8be48d431aeca778681c814b36bb986cba9b4
Vincent Pantia <vincent.m.pantia@hotmail.com>
2022-10-24 08:58:06 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+
+@section('content')
+    
+@stop
+
+
+@section('js')
+    <script src="{{ asset('js/app.js') }}"></script>
+@stop   
